d3ba76a34163523b079549f81e626cb34828486e
Manish Dudhwewala <MANAANSH001@e.ntu.edu.sg>
2026-09-08 16:40:40 +0800

    Clean stale project folder

--- GLC27_ITB15_5_FIXED/src/home-main.jsx
+++
@@ -1,4 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./styles.css";
-import HomePage from "./home.jsx";
-createRoot(document.getElementById("root")).render(<HomePage />);
--- GLC27_ITB15_5_FIXED/src/settings-main.jsx
+++
@@ -1,4 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./styles.css";
-import SettingsPage from "./settings.jsx";
-createRoot(document.getElementById("root")).render(<SettingsPage />);
